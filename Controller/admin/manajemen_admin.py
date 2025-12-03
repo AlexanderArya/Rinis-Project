0cfc25b15c49e3c838c44ad25a61cd551341144e
Andrean Bimo <bimowibowo4846@gmail.com>
2025-12-04 00:45:09 +0700

    Update insert untuk property

--- Controller/admin/manajemen_admin.py
+++ Controller/admin/manajemen_admin.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template,jsonify
+from flask import Blueprint, render_template, jsonify, request, session
+from Controller import mysql
 from utils.decorators import login_required
 from Model.landingPageModel import LandingPageModel
-from Model.adminModel import AdminModel 
+from Model.adminModel import AdminModel
+from Model.model_data import DatabaseCreate
 
-management_admin = Blueprint("management_admin",__name__,template_folder="../templates")
+management_admin = Blueprint("management_admin", __name__, template_folder="../templates")
 
 @management_admin.route("/managemet")
 @login_required
@@ -13,7 +15,22 @@ def admin_konten():
 @management_admin.route('/api/getAllData')
 @login_required
 def getAllData():
-    print("sudah masuk ke sini")
     data = AdminModel.getAllDataProperties()
-    print(data)
     return jsonify(data)
+
+@management_admin.route('/tambahData', methods=['POST'])
+@login_required
+def tambahan_data():
+    try:
+        data = request.get_json()
+        data['id_penjual'] = session['user']['id_user']
+        print(data)
+
+        # Memanggil fungsi statis dari DatabaseCreate tanpa membuat objek
+        result = DatabaseCreate.create_property(data)
+
+        return jsonify(result)
+    except Exception as e:
+        # Jika terjadi error, tangani exception dan kirimkan pesan error
+        print(f"Error: {e}")
+        return jsonify({'status': 'error', 'message': f'Gagal menambahkan properti: {str(e)}'}), 500
